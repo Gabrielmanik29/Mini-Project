fc968f276f5bc1ff15e489a768502067f856188f
Gabrielmanik29 <gabrielmanik2006@gmail.com>
2026-06-15 02:14:33 +0700

    Google Sign-In & Profil UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,4 @@
+import java.util.Properties
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
@@ -19,6 +20,10 @@ android {
         versionCode = 1
         versionName = "1.0"
 
+        val properties = Properties()
+        properties.load(project.rootProject.file("local.properties").inputStream())
+        buildConfigField("String", "API_KEY", properties.getProperty("API_KEY") ?: "\"\"")
+
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
@@ -36,6 +41,7 @@ android {
         targetCompatibility = JavaVersion.VERSION_11
     }
     buildFeatures {
+        buildConfig = true
         compose = true
     }
 }
---
+++ app/src/main/java/com/gabriel0011/asesmenmobpro/model/User.kt
@@ -0,0 +1,7 @@
+package com.gabriel0011.asesmenmobpro.model
+
+data class User(
+    val name: String = "",
+    val email: String = "",
+    val photoUrl: String = ""
+)
